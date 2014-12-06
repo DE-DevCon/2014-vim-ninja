eac2796340a7f99664c81c576ba68c6adb1c4197
Robert Bittle <guywithnose@gmail.com>
2014-12-05 23:46:51 -0500

    Rename demo files

---
+++ demos/3-dotCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+echo 'intergeneration';
+echo 'impressionistic';
+echo 'superrefinement';
+echo 'semispeculation';
+echo 'collenchymatous';
+echo 'punctiliousness';
+echo 'indigestibility';
+echo 'illegitimatised';
+echo 'hypersensualism';
+echo 'tendenciousness';
+echo 'antihierarchism';
+echo 'perchlorinating';
+echo 'premodification';
+echo 'nonprofessorial';
+echo 'quodlibetically';
+echo 'defencelessness';
+echo 'interchangeably';
+echo 'olericulturally';
+echo 'vespertilionine';
+echo 'unacknowledging';
